86fd9ecb18bbfd51efea30dc9fd4ce2071d86dcb
Pedro Sánchez <periko17188+proyecto@hotmail.com>
2026-05-20 12:03:08 +0000

    Arreglo cleaning_utils

--- macros/cleaning_utils.sql
+++ macros/cleaning_utils.sql
@@ -61,16 +61,18 @@ coalesce(
 
 {% macro clean_decimal(campo, precision=12, scale=2) -%}
 
+{% set campo_sin_unidad = "regexp_replace(lower(trim(" ~ campo ~ ")), '(€/m2|€/m²|eur_m2|eur/m2|eur/m²|euros/m2|euros/m²|euro/m2|euro/m²|/m2|/m²|m2|m²)', '')" %}
+
 try_to_decimal(
     case
         when {{ campo }} is null then null
         when nullif(trim({{ campo }}), '') is null then null
 
         -- Europeo con miles y coma decimal: 186.500,50 € -> 186500.50
-        when regexp_like(trim({{ campo }}), '^[^0-9-]*[0-9]{1,3}([.][0-9]{3})+[,][0-9]+[^0-9]*$')
+        when regexp_like({{ campo_sin_unidad }}, '^[^0-9-]*[0-9]{1,3}([.][0-9]{3})+[,][0-9]+[^0-9]*$')
             then replace(
                     replace(
-                        regexp_replace(trim({{ campo }}), '[^0-9,.-]', ''),
+                        regexp_replace({{ campo_sin_unidad }}, '[^0-9,.-]', ''),
                         '.',
                         ''
                     ),
@@ -79,23 +81,23 @@ try_to_decimal(
                  )
 
         -- Europeo con miles sin decimales: 186.500 € -> 186500
-        when regexp_like(trim({{ campo }}), '^[^0-9-]*[0-9]{1,3}([.][0-9]{3})+[^0-9]*$')
+        when regexp_like({{ campo_sin_unidad }}, '^[^0-9-]*[0-9]{1,3}([.][0-9]{3})+[^0-9]*$')
             then replace(
-                    regexp_replace(trim({{ campo }}), '[^0-9.-]', ''),
+                    regexp_replace({{ campo_sin_unidad }}, '[^0-9.-]', ''),
                     '.',
                     ''
                  )
 
-        -- Decimal con coma: 186500,50 -> 186500.50
-        when regexp_like(trim({{ campo }}), '.*,.*')
+        -- Decimal con coma: 786,94 -> 786.94
+        when regexp_like({{ campo_sin_unidad }}, '.*,.*')
             then replace(
-                    regexp_replace(trim({{ campo }}), '[^0-9,.-]', ''),
+                    regexp_replace({{ campo_sin_unidad }}, '[^0-9,.-]', ''),
                     ',',
                     '.'
                  )
 
         -- Formato estándar: 186500.50 o 186500
-        else regexp_replace(trim({{ campo }}), '[^0-9.-]', '')
+        else regexp_replace({{ campo_sin_unidad }}, '[^0-9.-]', '')
     end,
     {{ precision }},
     {{ scale }}
